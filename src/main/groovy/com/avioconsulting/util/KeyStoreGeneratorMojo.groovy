4c6ab6ddd02bf8b8d8e2586f73c872846dee6faf
Brady Wied <brady@wied.us>
2017-07-26 13:34:08 -0600

    Add our input parameters

--- src/main/groovy/com/avioconsulting/util/KeyStoreGeneratorMojo.groovy
+++ src/main/groovy/com/avioconsulting/util/KeyStoreGeneratorMojo.groovy
@@ -4,9 +4,17 @@ import org.apache.maven.plugin.AbstractMojo
 import org.apache.maven.plugin.MojoExecutionException
 import org.apache.maven.plugin.MojoFailureException
 import org.apache.maven.plugins.annotations.Mojo
+import org.apache.maven.plugins.annotations.Parameter
 
 @Mojo(name = 'generateKeystore')
 class KeyStoreGeneratorMojo extends AbstractMojo {
+    @Parameter(required = true, defaultValue = '${keystore.generated.path}')
+    private String destinationKeyStorePath
+    @Parameter(required = true, defaultValue = '${keystore.properties.file.path}')
+    private File keystorePasswordPropertiesFilePath
+    @Parameter(required = true, defaultValue = 'listener.keystore.password')
+    private String keystorePasswordPropertyName
+
     @Override
     void execute() throws MojoExecutionException, MojoFailureException {
 
