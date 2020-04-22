b05989ff368a71e6937faa993d30f8b7ac504d71
Brady Wied <brady@bswtechconsulting.com>
2020-04-22 14:27:36 -0600

    Clarify this

--- rebasepush.sh
+++ rebasepush.sh
@@ -4,19 +4,7 @@ set -e
 echo "Pushing to general AVIO Nexus"
 ./gradlew clean uploadArchives
 
-echo "Updating AVIO DFW bitbucket repo"
-# AVIO Nexus
-git checkout dfw_outside_network
+echo "Now updating customer code"
+git checkout customer_branch_name
 git rebase master
-git push --force
-
-echo "Pushing to AVIO Nexus DFW"
-./gradlew clean uploadArchives
-
-echo "Now updating DFW customer code"
-git checkout dfw
-git rebase master
-git push --force dfwgithub dfw:master
-git push --force origin dfw
-
-echo Now you can push to DFW Artifactory via Gradle on your VPN VM and then switch back...
+git push --force customer_remote_name customer_branch_name:master
